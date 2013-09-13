783c6906cf190a84cbec37d3737d8b4bae01910a
Dmitry Karpunin <koderfunk@gmail.com>
2013-09-13 19:38:54 +0400

    Fixes and improves

--- glider.js
+++ glider.js
@@ -1,23 +1,25 @@
 /*
- glider 0.0.2 - Angularjs slider
- https://github.com/Valve/glider
+ glider 0.0.3 - AngularJS slider
+ https://github.com/evrone/glider
  Copyright (c) 2013 Valentin Vasilyev, Dmitry Karpunin
  Licensed under the MIT (http://www.opensource.org/licenses/mit-license.php) license.
 */
 
 
 (function() {
-  var app,
-    __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
+  var app;
 
   app = angular.module("glider", []);
 
   app.directive("slider", [
     "$document", function($document) {
-      var moveHandle;
+      var getSubElement, moveHandle;
+      getSubElement = function(sliderElement, className) {
+        return sliderElement[0].getElementsByClassName(className)[0];
+      };
       moveHandle = function(elem, posX) {
-        angular.element(elem[0].getElementsByClassName('handle')[0]).css("left", "" + posX + "%");
-        return angular.element(elem[0].getElementsByClassName('range')[0]).css("width", "" + posX + "%");
+        angular.element(getSubElement(elem, "handle")).css("left", "" + posX + "%");
+        return angular.element(getSubElement(elem, "range")).css("width", "" + posX + "%");
       };
       return {
         template: "<span class=\"g-slider horizontal\">\n  <span class=\"slider\">\n    <span class=\"range\"></span>\n    <span class=\"handle\" ng-mousedown=\"mouseDown($event)\"></span>\n  </span>\n  <span class=\"side dec\">\n    <span class=\"button\" ng-click=\"step(-1)\">-</span>\n    <span class=\"bound-value\">{{min()}}</span>\n  </span>\n  <span class=\"side inc\">\n    <span class=\"button\" ng-click=\"step(+1)\">+</span>\n    <span class=\"bound-value\">{{max()}}</span>\n  </span>\n</span>",
@@ -25,44 +27,58 @@
         restrict: "E",
         scope: {
           value: "=",
-          min: '&',
-          max: '&'
+          min: "&",
+          max: "&"
         },
         link: function(scope, element, attrs) {
-          var dragging, startPointX, step, xPosition;
+          var dragging, refreshHandle, sliderElement, step, xPosition;
+          sliderElement = getSubElement(element, "slider");
           dragging = false;
-          startPointX = 0;
           xPosition = 0;
           step = attrs.step != null ? parseInt(attrs.step, 10) : 1;
           if (scope.value == null) {
             scope.value = scope.min();
           }
+          refreshHandle = function() {
+            var range;
+            range = scope.max() - scope.min();
+            if (range === 0) {
+              xPosition = 0;
+            } else {
+              xPosition = (scope.value - scope.min()) / range * 100;
+              xPosition = Math.min(Math.max(0, xPosition), 100);
+            }
+            return moveHandle(element, xPosition);
+          };
+          scope.$watch("min()", function(minValue) {
+            if (scope.value < minValue) {
+              return scope.value = minValue;
+            } else {
+              return refreshHandle();
+            }
+          });
+          scope.$watch("max()", function(maxValue) {
+            if (scope.value > maxValue) {
+              return scope.value = maxValue;
+            } else {
+              return refreshHandle();
+            }
+          });
           scope.$watch("value", function() {
             if (dragging) {
               return;
             }
-            xPosition = (scope.value - scope.min()) / (scope.max() - scope.min()) * 100;
-            if (xPosition < 0) {
-              xPosition = 0;
-            } else {
-              if (xPosition > 100) {
-                xPosition = 100;
-              }
-            }
-            return moveHandle(element, xPosition);
+            return refreshHandle();
           });
-          scope.step = function(step_value) {
-            var inc, _i, _ref, _ref1, _ref2, _results;
-            inc = step_value * step;
-            if (_ref = scope.value + inc, __indexOf.call((function() {
-              _results = [];
-              for (var _i = _ref1 = scope.min(), _ref2 = scope.max(); _ref1 <= _ref2 ? _i <= _ref2 : _i >= _ref2; _ref1 <= _ref2 ? _i++ : _i--){ _results.push(_i); }
-              return _results;
-            }).apply(this), _ref) >= 0) {
-              return scope.value += inc;
+          scope.step = function(steps) {
+            var newValue;
+            newValue = scope.value + steps * step;
+            if ((scope.min() <= newValue && newValue <= scope.max())) {
+              return scope.value = newValue;
             }
           };
           return scope.mouseDown = function($event) {
+            var startPointX;
             dragging = true;
             startPointX = $event.pageX;
             $document.on("mousemove", function($event) {
@@ -71,7 +87,7 @@
                 return;
               }
               moveDelta = $event.pageX - startPointX;
-              xPosition = xPosition + ((moveDelta / element[0].offsetWidth) * 100);
+              xPosition += moveDelta / sliderElement.offsetWidth * 100;
               if (xPosition < 0) {
                 xPosition = 0;
               } else if (xPosition > 100) {
@@ -83,7 +99,7 @@
               scope.$apply();
               return moveHandle(element, xPosition);
             });
-            return $document.on('mouseup', function() {
+            return $document.on("mouseup", function() {
               dragging = false;
               return $document.off("mousemove");
             });
